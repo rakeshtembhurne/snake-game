48decaf63d461e72701cc647d9b28384006f90a4
Rakesh Tembhurne <tj2point0@gmail.com>
2019-05-25 20:18:00 +0530

    Built a grid system

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      rows: 10,
+      cols: 10,
+      grid: [],
+    };
+  }
+
+  componentDidMount() {
+    const grid = [];
+    for (let row = 0; row < this.state.rows; row++) {
+      for (let col = 0; col < this.state.cols; col++) {
+        grid.push({
+          row,
+          col,
+          isFood: false,
+        })
+      }
+    }
+    this.setState({ grid: grid })
+  }
+  
+  render () {
+    const gridItems = this.state.grid.map((grid) => {
+      return <div 
+        key={grid.row.toString() + '-' + grid.col.toString()} 
+        className="gridItem" ></div>
+    })
+    return (
+      <div className="snake-container">
+        <div className="grid">{gridItems}</div>
+      </div>
+    )
+  }
 }
 
 export default App;
