e0de3266da5a5f13f1478139353c797f7ca93fe5
Rakesh Tembhurne <tj2point0@gmail.com>
2019-05-25 21:49:54 +0530

    Food now appears at random location

--- src/App.js
+++ src/App.js
@@ -9,28 +9,43 @@ class App extends React.Component {
       rows: 10,
       cols: 10,
       grid: [],
+      food: {row: 5, col: 4}
     };
   }
 
+  getRandomGrid() {
+    return {
+      row: Math.floor((Math.random() * 10) + 1),
+      col: Math.floor((Math.random() * 10) + 1)
+    }
+  }
+
   componentDidMount() {
+    // Food 
+    const food = this.getRandomGrid();
+    
+    // Grid System
     const grid = [];
+    
     for (let row = 0; row < this.state.rows; row++) {
       for (let col = 0; col < this.state.cols; col++) {
+        const isFood = (food.row === row && food.col === col);
         grid.push({
           row,
           col,
-          isFood: false,
+          isFood,
         })
       }
     }
     this.setState({ grid: grid })
+
   }
   
   render () {
     const gridItems = this.state.grid.map((grid) => {
       return <div 
         key={grid.row.toString() + '-' + grid.col.toString()} 
-        className="gridItem" ></div>
+        className={grid.isFood ? 'gridItem is-food' : 'gridItem'} ></div>
     })
     return (
       <div className="snake-container">
