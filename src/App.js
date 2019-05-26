459ec1962c67484dede2f583333a491a2f5cc9c9
Rakesh Tembhurne <tj2point0@gmail.com>
2019-05-26 06:37:52 +0530

    Added snake which moves in default direction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
-  
-  constructor() {
-    super();
-    this.state = {
-      rows: 10,
-      cols: 10,
-      grid: [],
-      food: {row: 5, col: 4}
-    };
-  }
+// const log = data => console.log(JSON.stringify(data, null, 2));
 
+class App extends React.Component {
+
+  state = {
+    tickTime: 2000,
+    rows: 10,
+    cols: 10,
+    grid: [],
+    food: {},
+    snake: {
+      head: {},
+    },
+    currentDirection: 'right'
+  };
+  
   getRandomGrid() {
     return {
       row: Math.floor((Math.random() * 10) + 1),
@@ -20,32 +24,83 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount() {
-    // Food 
-    const food = this.getRandomGrid();
-    
-    // Grid System
+  // TODO: snake and food begins at the center
+  getCenterOfGrid() {
+    return {
+      row: Math.floor((this.state.rows - 1) / 2),
+      col: Math.floor((this.state.cols - 1) / 2),
+    }
+  }
+
+  resetGrid() {
     const grid = [];
-    
-    for (let row = 0; row < this.state.rows; row++) {
-      for (let col = 0; col < this.state.cols; col++) {
+    const { rows, cols, food, snake } = this.state;
+
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
         const isFood = (food.row === row && food.col === col);
+        const isHead = (snake.head.row === row && snake.head.col === col);
         grid.push({
           row,
           col,
           isFood,
+          isHead,
         })
       }
     }
-    this.setState({ grid: grid })
 
+    this.setState({ grid })
+  }
+
+  gameTick() {
+    const { currentDirection } = this.state;
+    
+    // Snake keeps moving
+    switch (currentDirection) {
+      case 'right':
+        this.setState((state) => {
+          return {
+            snake: {
+              head: {
+                row: state.snake.head.row,
+                col: state.snake.head.col + 1,
+              },
+            }
+          }
+        });
+        break;
+
+      default:
+        break;
+    }
+    this.resetGrid();
+  }
+
+  componentDidMount() {
+    // Food 
+    this.setState({ food: this.getRandomGrid() });
+
+    this.setState({
+      snake: {
+        head: this.getCenterOfGrid()
+      }
+    });
+    
+    this.gameTick();
+
+    setInterval(() => {
+      this.gameTick();
+    }, this.state.tickTime);
   }
   
   render () {
     const gridItems = this.state.grid.map((grid) => {
       return <div 
         key={grid.row.toString() + '-' + grid.col.toString()} 
-        className={grid.isFood ? 'gridItem is-food' : 'gridItem'} ></div>
+        className={
+          grid.isHead ? 'gridItem is-head' : grid.isFood ? 'gridItem is-food' : 'gridItem'
+        }
+      ></div>
     })
     return (
       <div className="snake-container">
