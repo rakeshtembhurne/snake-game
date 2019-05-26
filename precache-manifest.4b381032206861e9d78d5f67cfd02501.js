e6218e9dfbdcbb9d00c1b1913ab687eb52333eb8
Rakesh Tembhurne <tj2point0@gmail.com>
2019-05-26 21:53:38 +0530

    Updates

---
+++ precache-manifest.4b381032206861e9d78d5f67cfd02501.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afc4df9fe3642607d7034d99a892ce4",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "b5b900c65b4d90c33edf",
+    "url": "/snake-game/static/css/main.c4dbc200.chunk.css"
+  },
+  {
+    "revision": "daa31f6663532aabb2ee",
+    "url": "/snake-game/static/js/2.d190dc04.chunk.js"
+  },
+  {
+    "revision": "b5b900c65b4d90c33edf",
+    "url": "/snake-game/static/js/main.b2fd4322.chunk.js"
+  },
+  {
+    "revision": "9a755d0ffde03a58c68a",
+    "url": "/snake-game/static/js/runtime~main.356adb96.js"
+  }
+]);
