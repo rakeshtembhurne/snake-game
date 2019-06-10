7f35dab2c3f8486dd1025c19feb7aa33d896693e
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-10 15:15:05 +0530

    Adding in missing config file

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+  },
+  apiGateway: {
+    REGION: "ap-south-1",
+    URL: "https://4xtfqw9ah9.execute-api.ap-south-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-south-1",
+    USER_POOL_ID: "ap-south-1_EBEF554a8",
+    APP_CLIENT_ID: "7kn3mib6vn1u7nfsnp8mlhspjt",
+    IDENTITY_POOL_ID: "ap-south-1:2bd35c0f-75e1-4b46-9bf7-633d4f44189b"
+  }
+};
