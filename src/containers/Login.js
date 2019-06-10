9d79e86c503e4401ea990bb6881a9bcf4c62c0be
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-10 15:30:19 +0530

    Created a demo user, shown credentials at login page

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -8,8 +8,8 @@ export default class Login extends Component {
     super(props);
 
     this.state = {
-      email: "",
-      password: "",
+      email: "demo@demo.com",
+      password: "demoDEMOdemo@123",
     }
   }
 
